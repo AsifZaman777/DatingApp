8eaeb7135f756981f728cbda76c23ee09800e0b7
asif.zaman <asif.zaman@LBSCOR235>
2024-10-06 16:35:34 +0600

    Add project files.

---
+++ API/Models/AppUser.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Models
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public required string Username { get; set; } // string is a reference type
+
+    }
+}
