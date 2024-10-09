9997682041b04303e1aefc48a117fa7a180372cc
Asif Zaman <eramasif16@gmail.com>
2024-10-09 15:14:00 +0600

    controller based api created to get users

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,11 +1,64 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using API.Data;
+using API.Models;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace API.Controllers;
 
 [ApiController]
-[Route("api/[controller]")] //api/users
+[Route("api/[controller]")] //api/users endpoint
 
-public class UsersController : ControllerBase
+
+//conventional way 
+
+//public class UsersController : ControllerBase
+//{
+//    private readonly DataContext context;
+
+//    public UsersController(DataContext context)
+//    {
+//      this.context = context;
+//    }
+//    [HttpGet]
+//    public ActionResult<IEnumerable<AppUser>> GetUsers()
+//    {
+
+//    }
+//}
+
+
+// c#12 way using primary constructor
+public class UsersController(DataContext context) : ControllerBase
 {
-    
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUser()
+    {
+        var users = await context.Users.ToListAsync();
+        return Ok(users);
+    }
+
+    //search by id
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        var user = await context.Users.FindAsync(id);
+        if(user == null) return NotFound();
+        return Ok(user);
+    }
+
+    //search by name
+    [HttpGet("{name}")]
+    public async Task<ActionResult> GetUser(string name)
+    {
+        var user = await context.Users.FirstOrDefaultAsync(u => u.Username.ToLower() == name.ToLower());
+
+        if (user == null) return NotFound();
+        return Ok(user);
+
+
+    }
+
 }
+
+
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] // Fixed 'styleUrl' to 'styleUrls'
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
+  title = 'Dating app';
+  users: any;
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5069/api/users').subscribe(
+      {
+        next: response => this.users = response,
+        error: error => console.log(error),
+        complete: () => console.log('complete')
+      }
+    );
+  } 
 }
